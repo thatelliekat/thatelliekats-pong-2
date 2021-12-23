3fd9e98daa0bd540a589601c96b258d936894e65
thatelliekat <96074425+thatelliekat@users.noreply.github.com>
2021-12-23 13:55:40 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,7 +52,7 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("point get.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("Adventure_Meme.mp3");
+export var ambientSound = new soundFile("Adventure_Meme_2.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -5; //turn down volume
